26dbba23f45f78b66fdc5cd9a9ec66b48340cf6d
Antonio Fernando Santos Ladeia <contato@antonioladeia.com>
2017-04-18 15:54:37 -0300

    brincadeiras com o rgb
    brincadeiras com o rgb

--- rgbpwm/rgbpwm.ino
+++ rgbpwm/rgbpwm.ino
@@ -1,32 +1,56 @@
-int ledr = 13;
-int ledg = 12;
-int ledb = 11;
-int brigtness = A0; //10; // Use Pin 10 as our Input
+int ledr = 11;
+int ledg = 13;
+int ledb = 12;
+int brigtness = 9;//A0; //10; // Use Pin 10 as our Input
 int boobs;
+int cycle;
+int r;
+int g;
+int b;
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(ledr, OUTPUT);
   pinMode(ledg, OUTPUT);
   pinMode(ledb, OUTPUT);
   pinMode (brigtness, OUTPUT) ;
-  boobs = 50;
+  boobs = 0;
+  cycle = 0;
+  r= HIGH;
+  g= HIGH;
+  b= HIGH;  
   Serial.begin(9600);  
 }
 
 // the loop function runs over and over again forever
 void loop() {
   boobs += 50;
-  Serial.println(boobs);
-  analogWrite(ledr, 10);   // turn the LED on (HIGH is the voltage level)
-  analogWrite(ledg, 10);   // turn the LED on (HIGH is the voltage level)
-  analogWrite(ledb, 10);   // turn the LED on (HIGH is the voltage level)
+  
+  if(cycle == 0){
+    r = LOW;
+    g= HIGH;
+    b = HIGH;
+    }else if(cycle == 1){
+    r = HIGH;
+    g= LOW;
+    b = HIGH;
+    }else{
+      r = HIGH;
+      g= HIGH;
+      b = LOW;
+      }
+  
+  digitalWrite(ledr, r);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(ledg, g);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(ledb, b);   // turn the LED on (HIGH is the voltage level)
 
   analogWrite(brigtness, boobs);
   
   delay(1000);                       // wait for a second
 
-  if(boobs >1000)
-    boobs = 50;
+  if(boobs >= 250){
+    boobs = 0;
+    cycle = (cycle+1)%3;
+  }
  /* analogWrite(ledr, 0);   // turn the LED on (HIGH is the voltage level)
   analogWrite(ledg, 25);   // turn the LED on (HIGH is the voltage level)
   analogWrite(ledb, 50);   // turn the LED on (HIGH is the voltage level)
