aabf7a035b6021bf5ff29e09d827109c3b4627d2
Antonio Fernando Santos Ladeia <contato@antonioladeia.com>
2017-05-12 13:54:54 -0300

    Interação entre bluetooth e leds

--- bt-rgb/bt-rgb.ino
+++ bt-rgb/bt-rgb.ino
@@ -1,19 +1,25 @@
 #include <SoftwareSerial.h>
 
+int green = 12;
+int red = 9;
+int blue = 13;
 SoftwareSerial mySerial(10, 11); // RX, TX
-String command = "";
+String command;
 void setup()
 {
+  pinMode(blue, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
   // Open serial communications and wait for port to open:
   Serial.begin(115200);
   Serial.println("Type AT commands!");
   // SoftwareSerial "com port" data rate. JY-MCU v1.03 defaults to 9600.
   mySerial.begin(9600);
-  
 }
 
 void loop()
 {
+
   // Read device output if available.
   if (mySerial.available())
   {    while (mySerial.available())
@@ -21,6 +27,23 @@ void loop()
       command += (char)mySerial.read();
     }
     Serial.print(command);
+    if(command == "0"){
+         digitalWrite(blue,LOW);
+      digitalWrite(red ,LOW);
+      digitalWrite(green, HIGH);
+    }else if (command == "1") {
+         digitalWrite(blue,LOW);
+      digitalWrite(red ,HIGH);
+      digitalWrite(green, LOW);
+    }else if(command == "2"){
+         digitalWrite(blue,HIGH);
+      digitalWrite(red ,LOW);
+      digitalWrite(green, LOW);
+    } else{
+               digitalWrite(blue,LOW);
+      digitalWrite(red ,LOW);
+      digitalWrite(green, LOW);
+      }
     command = ""; // No repeats
   }
   // Read user input if available.
