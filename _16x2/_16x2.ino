113cfa7865aedf0aedf634089a35c55a63b60b17
Antonio Fernando Santos Ladeia <contato@antonioladeia.com>
2017-05-25 13:12:06 -0300

    Testes com biblioteca de lcd

--- _16x2/_16x2.ino
+++ _16x2/_16x2.ino
@@ -1,44 +1,42 @@
 //Programa: Teste de Display LCD 16 x 2
 //Autor: FILIPEFLOP
- 
-//Carrega a biblioteca LiquidCrystal
+
+int const MAX_SCREEN_SIZE = 16;
 #include <LiquidCrystal.h>
  
-//Define os pinos
-int defaultChar = 65; que serão utilizados para ligação ao display
+//Define os pinos que serão utilizados para ligação ao display
+String defaultChar = "B*"; 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 String message = "";
-//
-int currentChar = 0;
 
 void setup()
 {
   //Define o número de colunas e linhas do LCD
   lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
 }
  
 void loop()
 {
-  if(message == "")
-    currentChar = defaultChar;
-  
-  //Limpa a tela
   lcd.clear();
-  lcd.setCursor(3, 0);
-  lcd.print(currentChar);
-  delay(5000);
-   
-  //Rolagem para a esquerda
-  for (int posicao = 0; posicao < 3; posicao++)
+  lcd.print(defaultChar);
+
+  moveRight(MAX_SCREEN_SIZE * 2);
+}
+
+void moveLeft(int positions) {
+  for (int posicao = 0; posicao < positions; posicao++)
   {
     lcd.scrollDisplayLeft();
     delay(300);
   }
-   
-  //Rolagem para a direita
-  for (int posicao = 0; posicao < 6; posicao++)
+}
+
+void moveRight(int positions) {
+  for (int posicao = 0; posicao < positions; posicao++)
   {
     lcd.scrollDisplayRight();
     delay(300);
   }
 }
+
